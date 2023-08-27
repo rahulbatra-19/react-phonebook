e865631ee87d8c195dc319d38dc21fd510a3eb41
rahulbatra-19 <batrarahul409@gmail.com>
2023-08-28 01:09:07 +0530

    Data fetched from api and created Contact card component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/app.module.css";
+import ContactCard from "./ContactCard";
+
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiurl = "https://jsonplaceholder.typicode.com/users";
+        const response = await fetch(apiurl);
+        const contacts = await response.json();
+        console.log(contacts);
+        setData(contacts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="App">
+      <div className={styles.Main}>
+        {data.map((contact, index) => (
+          <ContactCard contact={contact} key={`Contact-${index}`} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
